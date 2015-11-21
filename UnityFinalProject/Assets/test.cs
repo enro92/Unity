25370bbe319e54d2fbe8b816ecc02b9dc1284be8
enro92 <enro92@hotmail.com>
2015-11-21 11:23:46 +0700

    First Commit

---
+++ UnityFinalProject/Assets/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+<<<<<<< HEAD
+		int i = 7;
+
+=======
+		int i = 3;
+>>>>>>> c261bc1f5e59b2327e9871a0b7af65c2cebda674
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

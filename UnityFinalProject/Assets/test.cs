8f9dde9222e402a6ae07f4e7cc57f0c21dfeae81
enro92 <enro92@hotmail.com>
2015-11-21 11:30:06 +0700

    Second Commit

--- UnityFinalProject/Assets/test.cs
+++ UnityFinalProject/Assets/test.cs
@@ -5,14 +5,11 @@ public class test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
-		int i = 7;
 
-=======
+
 		int i = 3;
->>>>>>> c261bc1f5e59b2327e9871a0b7af65c2cebda674
+
 	}
-	
 	// Update is called once per frame
 	void Update () {
 	
